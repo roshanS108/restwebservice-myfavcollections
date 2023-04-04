c0d3d84d4db3e599187d86295301780be9d3bb26
roshankumar811 <roshanshoti100@gmail.com>
2023-04-04 09:57:00 -0400

    adding security service layer to only allow specific person to perform crud operation

--- src/main/java/com/project/restcrud/security/WebSecurityConfigure.java
+++ src/main/java/com/project/restcrud/security/WebSecurityConfigure.java
@@ -1,23 +1,43 @@
 package com.project.restcrud.security;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.jaas.memory.InMemoryConfiguration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-
+import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class WebSecurityConfigure {
-
     @Bean
-    public InMemoryUserDetailsManager userDetailManager(){
+    public InMemoryUserDetailsManager userDetailManager() {
         UserDetails roshan = User.builder()
                 .username("roshan")
                 .password("{noop}test123")
                 .roles("Owner")
                 .build();
-
-        return new InMemoryUserDetailsManager(roshan);
+        UserDetails guest = User.builder()
+                .username("guest")
+                .password("{noop}test123")
+                .roles("Guest")
+                .build();
+        return new InMemoryUserDetailsManager(roshan,guest);
     }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/book/favoriteBooks").hasRole("Guest")
+                        .requestMatchers(HttpMethod.GET, "/book/favoriteBooks/**").hasRole("Guest")
+                        .requestMatchers(HttpMethod.POST, "/book/favoriteBooks").hasRole("Owner")
+                        .requestMatchers(HttpMethod.PUT, "/book/favoriteBooks").hasRole("Owner")
+                        .requestMatchers(HttpMethod.DELETE, "/book/favoriteBooks/**").hasRole("Owner")
+        );
+        //use http basic authentication
+        http.httpBasic();
+        //disable cross site request forgery (CSRF)
+        http.csrf().disable();
+        return http.build();
+    }
+
 }
