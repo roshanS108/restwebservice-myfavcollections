c56e044dbc9082aafbb3695e3e5b5d4d38708e38
roshankumar811 <roshanshoti100@gmail.com>
2023-04-05 19:49:00 -0400

    Added more fields in Book class

--- src/main/java/com/project/restcrud/entity/Book.java
+++ src/main/java/com/project/restcrud/entity/Book.java
@@ -1,30 +1,75 @@
 package com.project.restcrud.entity;
-
 import jakarta.persistence.*;
-
+import java.sql.Date;
 @Entity
 @Table(name = "Book")
 public class Book {
-
     //define fields
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id_book")
     private int id;
 
-    @Column(name = "first_name")
+    @Column(name = "book_name")
     private String bookName;
 
-    @Column(name = "last_name")
+    @Column(name = "name_of_author")
     private String authorName;
+
+    @Column(name = "publisher_of_book")
+    private String publisher;
+
+    @Column(name = "book_isbn")
+    private String isbn;
+
+    @Column(name = "book_release_date")
+    private String publicationDate;
+
+    @Column(name = "genre")
+    private String genre;
 
     public Book(){
 
     }
-
-    public Book(String bookName, String authorName) {
+    public Book(String bookName, String authorName,String publisher, String isbn, String publicationDate, String genre) {
         this.bookName = bookName;
         this.authorName = authorName;
+        this.publisher = publisher;
+        this.isbn = isbn;
+        this.publicationDate = publicationDate;
+        this.genre = genre;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(String publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
     }
 
     public int getId() {
--- src/main/java/com/project/restcrud/jparepository/MyRepository.java
+++ src/main/java/com/project/restcrud/jparepository/MyRepository.java
@@ -1,7 +1,6 @@
 package com.project.restcrud.jparepository;
-
+import com.project.restcrud.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MyRepository<T> extends JpaRepository<T, Integer> {
+public interface MyRepository extends JpaRepository<Book, Integer>{
 
 }
