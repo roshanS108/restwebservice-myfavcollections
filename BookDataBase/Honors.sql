a5fcda4505130f14f98be2e90d60b42292ba3840
roshankumar811 <roshanshoti100@gmail.com>
2023-04-07 22:57:21 -0400

    adding honors table

--- BookDataBase/Honors.sql
+++
@@ -1,8 +0,0 @@
-USE my_fav_collection;
-
-CREATE TABLE IF NOT EXISTS Honors (
-    id INT NOT NULL AUTO_INCREMENT,
-    name_of_tournament VARCHAR(45) NOT NULL,
-    times_won INT NOT NULL,
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
