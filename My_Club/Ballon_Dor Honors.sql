efdaf06aad02ed28a3ab3c8f222db112b4b5c017
roshankumar811 <roshanshoti100@gmail.com>
2023-04-08 21:49:29 -0400

    added one more foreign key of club in ballon'dor winner list

--- My_Club/Ballon_Dor Honors.sql
+++ My_Club/Ballon_Dor Honors.sql
@@ -1,18 +1,22 @@
 USE `my_fav_collection`;
 create table Ballon_Dor_Honors(
 
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `age` INT DEFAULT NULL,
-  `name` VARCHAR(100) NOT NULL,
-  `nationality` VARCHAR(50) NOT NULL,
-  `club` VARCHAR(100) DEFAULT NULL,
-  `goals_that_season` INT DEFAULT NULL,
-  `assists_that_season` INT DEFAULT NULL,
-  `trophies_that_season` INT DEFAULT NULL,
-  `year` INT DEFAULT NULL,
-  `Champion_League_Win` INT DEFAULT NULL,
+   `id` int NOT NULL AUTO_INCREMENT,
+    `age` int DEFAULT NULL,
+    `name` varchar(100) NOT NULL,
+    `nationality` varchar(50) NOT NULL,
+    `goals_that_season` int DEFAULT NULL,
+    `assists_that_season` int DEFAULT NULL,
+    `years` int DEFAULT NULL,
+    `champion_league_win` int DEFAULT NULL,
+    `club` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `fk_soccer_player_honorss` (`champion_league_win`),
+    KEY `fk_club_player_balldor` (`club`),
+    CONSTRAINT `fk_club_player_balldor` FOREIGN KEY (`club`) REFERENCES `club` (`id`),
+    CONSTRAINT `fk_soccer_player_honorss` FOREIGN KEY (`champion_league_win`) REFERENCES `honors` (`id`)
 
-  FOREIGN KEY (`Champion_League_Win`) REFERENCES `honors` (`id`),
+
 
   PRIMARY KEY (`id`)
 
