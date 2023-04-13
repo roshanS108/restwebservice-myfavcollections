72d92127ec75ddcb82705678540b935ea58ebd3a
roshankumar811 <roshanshoti100@gmail.com>
2023-04-13 14:38:22 -0400

    added this interface to do some testing in redis cache memory and implementing this method to service layer

---
+++ src/main/java/com/project/restcrud/redis_cache_Service/DemoRedisCache.java
@@ -0,0 +1,11 @@
+package com.project.restcrud.redis_cache_Service;
+
+import com.project.restcrud.entity.Book.Book;
+
+import java.util.List;
+
+//this interface was made just to do some testing in redis cache
+public interface DemoRedisCache {
+
+    List<Book> fetchAllBooks();
+}
