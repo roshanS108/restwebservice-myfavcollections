e4f5d350764fb0a0a09c64f94fb1f93b232eaed5
roshankumar811 <roshanshoti100@gmail.com>
2023-04-13 14:55:25 -0400

    implemented the fetchAllBooks in BookServiceImpl class

--- src/main/java/com/project/restcrud/service/BookServiceImpl.java
+++ src/main/java/com/project/restcrud/service/BookServiceImpl.java
@@ -1,54 +1,76 @@
 package com.project.restcrud.service;
+import com.project.restcrud.cache.CacheService;
 import com.project.restcrud.entity.Book.Book;
 import com.project.restcrud.jparepository.MyRepository;
+import com.project.restcrud.redis_cache_Service.DemoRedisCache;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
-
 @Service
-public class BookServiceImpl implements BookService{
+public class BookServiceImpl implements BookService, DemoRedisCache {
+    private MyRepository myRepository;
 
-    private MyRepository bookService;
+    //Normal --> Cache without redis
+    private CacheService cacheService;
 
+    private RedisTemplate redisTemplate;
+
+    private static final String KEY = "USER";
     @Autowired
-    public BookServiceImpl(MyRepository bookService){
-        this.bookService = bookService;
+    public BookServiceImpl(MyRepository bookService, RedisTemplate redisTemplate){
+        this.myRepository = bookService;
+        this.redisTemplate = redisTemplate;
     }
+    //getting all the books from the database ---> Not using any redis cache to store the book
     @Override
     public List<Book> findAll() {
-        return bookService.findAll();
+        return myRepository.findAll();
     }
-
     @Override
+    @Cacheable(value = "finding book by ID")
     public Book findById(int theID) {
-        Optional<Book>theResult = bookService.findById(theID);
-
+        //if the data is already in the cache memory just return that whatever in the cache memory
+        if(cacheService.containsBookInCache(theID)){
+            System.out.println("the data is already in cache");
+            return cacheService.getBook(theID);
+        }
+        Optional<Book>theResult = myRepository.findById(theID);
         Book theBook = null;
+        //if the data is not  in the cache memory add that data into cache memory.
         if(theResult.isPresent()){
             theBook = theResult.get();
+            System.out.println("This method is being called");
+            cacheService.putBook(theID,theBook);
+            /*Book theBook2 = cacheService.getBook(theID);
+            System.out.println("the book name: " + theBook2.getBookName());*/
         }
         return theBook;
     }
-
     @Transactional
     @Override
     public Book saveBook(Book theBook) {
-        return bookService.save(theBook);
-    }
+       /* try{
+            redisTemplate.opsForHash().put(KEY, theBook.getId(), theBook);
 
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+
+        return myRepository.save(theBook);
+    }
     @Transactional
     @Override
     public void deleteById(int theId) {
-        bookService.deleteById(theId);
+        myRepository.deleteById(theId);
     }
-
     @Override
     public Book findByName(String name) {
-        Optional<Book> theResult = Optional.ofNullable(bookService.findByBookNameIgnoreCase(name));
+        Optional<Book> theResult = Optional.ofNullable(myRepository.findByBookNameIgnoreCase(name));
 //        Book theResult = bookService.findByName(name);
-
         Book theBook = null;
         if(theResult.isPresent()){
             theBook = theResult.get();
@@ -58,7 +80,7 @@ public class BookServiceImpl implements BookService{
     @Override
     public Book findByBookAuthor(String authorName) {
 
-        Optional<Book> theResult = Optional.ofNullable(bookService.findBookByAuthorNameIgnoreCase(authorName));
+        Optional<Book> theResult = Optional.ofNullable(myRepository.findBookByAuthorNameIgnoreCase(authorName));
         Book theBook = null;
         if(theResult.isPresent()){
             theBook = theResult.get();
@@ -67,21 +89,30 @@ public class BookServiceImpl implements BookService{
     }
     @Override
     public Book findBookByIsbn(String bookIsbn) {
-        Optional<Book> theResult = Optional.ofNullable(bookService.findBookByISBNIgnoreCase(bookIsbn));
+        Optional<Book> theResult = Optional.ofNullable(myRepository.findBookByISBNIgnoreCase(bookIsbn));
         Book theBook = null;
         if(theResult.isPresent()){
             theBook = theResult.get();
         }
         return theBook;
     }
-
     @Override
     public Book findBookByGenre(String genre) {
-        Optional<Book> theResult = Optional.ofNullable(bookService.findBookByGenreIgnoreCase(genre));
+        Optional<Book> theResult = Optional.ofNullable(myRepository.findBookByGenreIgnoreCaseLike(genre));
         Book theBook = null;
         if(theResult.isPresent()){
             theBook = theResult.get();
         }
+        return theBook;
+    }
+    /*method from interface --> Demo Redis Cache
+    * it fetch all the books from the redis cache
+    *
+    * */
+    @Override
+    public List<Book> fetchAllBooks() {
+        List<Book> theBook;
+        theBook = redisTemplate.opsForHash().values(KEY);
         return theBook;
     }
 }
